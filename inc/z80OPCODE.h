980c72377dcc83811daa89208da30e84b330ae83
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-09 14:13:42 +0000

    added more masks

--- inc/z80OPCODE.h
+++ inc/z80OPCODE.h
@@ -27,4 +27,26 @@
     #define     OPCODE_BIT_MASK         0xFF
     #define     OPCODE_MAX              0x100
 
+    // OPCODE DECODING MACROS - USED FOR BIT SHIFTS
+    // Y = 5-3, Z = 2-0, P = 5-4, Q = 4-3   
+
+    #define     OPCODE_Y(VALUE)         (((OPCODE) >> 3) & 0x07)
+    #define     OPCODE_Z(VALUE)         ((OPCODE) & 0x07)
+    #define     OPCODE_P(VALUE)         (((OPCODE) >> 4) & 0x03)
+    #define     OPCODE_Q(VALUE)         (((OPCODE) >> 3) & 0x03)
+
+    #define     OPCODE_LD_A_I           0x57
+    #define     OPCODE_LD_I_A           0x47
+
+    #define     OPCODE_LDI              0xa0    
+    #define     OPCODE_LDIR             0xb0
+    #define     OPCODE_CPI              0xa1
+    #define     OPCODE_CPIR             0xb1
+
+    #define     OPCODE_RLD              0x6f
+
+
+    U8 Z80_GET_OPCODE_CYCLES(U8, U8);
+
+#endif
 #endif
