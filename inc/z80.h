22d23aa24e9dbc8d8ca2617ba42ffe01e8c2a45c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-09 13:42:57 +0000

    Update z80.h

--- inc/z80.h
+++ inc/z80.h
@@ -63,5 +63,43 @@
 #define         Z80_READ_BYTE(VALUE, ADDRESS)               (VALUE)[(ADDRESS) ^ 1]
 #define         Z80_READ_WORD(VALUE, ADDRESS)               (((VALUE)[ADDRESS]<<8) | (VALUE)[(ADDRESS)+1])
 
-#endif
+#define         Z80_LOW_NIBBLE(VALUE)
+#define         Z80_HIGH_NIBBLE(VALUE)                             ((VALUE >> 9) << 16)
+
+#define         Z80_B                   0
+#define         Z80_C                   1
+#define         Z80_D                   2
+#define         Z80_E                   3
+#define         Z80_H                   4
+#define         Z80_L                   5
+#define         Z80_A                   6
+#define         Z80_F                   7
+#define         Z80_IXH                 8
+#define         Z80_IXL                 9
+#define         Z80_IYH                 10  
+#define         Z80_IYL                 11
+
+#define         Z80_S_FLAG_SHIFT        7       
+#define         Z80_Z_FLAG_SHIFT        6
+#define         Z80_Y_FLAG_SHIFT        5
+#define         Z80_H_FLAG_SHIFT        4
+#define         Z80_X_FLAG_SHIFT        3
+#define         Z80_PV_FLAG_SHIFT       2
+#define         Z80_N_FLAG_SHIFT        1
+#define         Z80_C_FLAG_SHIFT        0
+
+#define         Z80_S_FLAG              (1 << Z80_S_FLAG_SHIFT)
+#define         Z80_Z_FLAG              (1 << Z80_Z_FLAG_SHIFT)
+#define         Z80_Y_FLAG              (1 << Z80_Y_FLAG_SHIFT)
+#define         Z80_H_FLAG              (1 << Z80_H_FLAG_SHIFT)
+#define         Z80_X_FLAG              (1 << Z80_X_FLAG_SHIFT)
+#define         Z80_PV_FLAG             (1 << Z80_PV_FLAG_SHIFT)
+#define         Z80_N_FLAG              (1 << Z80_N_FLAG_SHIFT)
+#define         Z80_C_FLAG              (1 << Z80_C_FLAG_SHIFT)
+
+#define         Z80_P_FLAG_SHIFT        Z80_PV_FLAG_SHIFT
+#define         Z80_V_FLAG_SHIFT        Z80_PV_FLAG_SHIFT
+#define         Z80_P_FLAG              Z80_PV_FLAG
+#define         Z80_V_FLAG              Z80_PV_FLAG
+
 #endif
