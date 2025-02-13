f5cb12e9260201ee7ab44dd55c6eeb79858e05c8
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-13 19:46:21 +0000

    added test functions for executing opcodes

--- src/main.c
+++ src/main.c
@@ -8,18 +8,111 @@
 #include "common.h"
 #include "z80.h"
 #include "z80OPCODE.h"
-#include "z80CONF.h"
 
 // SYSTEM INCLUDES
 
 #include <stdio.h>
 #include <stdlib.h>
 
+// AS OF RIGHT NOW, I AM TESTING THE WATERS REGARDING WHICH OPCODES WORK AND WHAT HAVE YOU
+// OF COURSE, BEING AS THOUGH THAT THE OPCODE LIST IS DIRECTLY FROM LIB68K, THE SCHEMA HAS BEEN TRIED AND TESTED
+
+// AS OF RIGHT NOPW, I AM DOING A SIMPLE LD IMM TO EACH RESPECTIVE REGISTER AND RECORDING THEIR READ VALUE
+// THIS IS ALL WITHIN CONJUNCTION OF WHICH OPCODES ARE BEING EXECUTED IN IT'S DESIGNATED FUNCTION
+
+void EXECUTE_OPCODE(CPU_Z80* Z, U8 OPCODE)
+{
+    switch (OPCODE)
+    {
+        case 0x00:
+            printf("NOP\n");
+            break;
+
+        // LD A, N
+        case 0x3E:
+            printf("LD A, 0x%02X\n", READ_8(Z, Z->PC));
+            Z80_SET_REGISTERS(Z, Z80_A, READ_8(Z, Z->PC)); 
+            Z->PC++;
+            break;
+
+        // LD B, N
+        case 0x06:
+            printf("LD B, 0x%02X\n", READ_8(Z, Z->PC));
+            Z80_SET_REGISTERS(Z, Z80_B, READ_8(Z, Z->PC));
+            Z->PC++;
+            break;
+
+        // LD C, N
+        case 0x0E:
+            printf("LD C, 0x%02X\n", READ_8(Z, Z->PC));
+            Z80_SET_REGISTERS(Z, Z80_C, READ_8(Z, Z->PC)); 
+            Z->PC++;
+            break;
+
+        // HALT
+        case 0x76:
+            printf("HALT\n");
+            Z->HALT = 1;
+            break;
+
+        default: break;
+    }
+
+    Z80_OUTPUT(Z);
+}
+
+// SIMPLE LOAD INTO MEMORY IN ACCORDANCE WITH THE MAX SIZE
+
+void LOAD_TEST_PROGRAM(CPU_Z80* Z, U8* PROGRAM, U16 SIZE)
+{
+    for (U16 INDEX = 0; INDEX < SIZE; INDEX++)
+    {
+        U16 ADDR = 0x0000 + INDEX;
+        WRITE_8(Z, ADDR, PROGRAM[INDEX]);
+    }
+}
+
 int main(void)
 {
     printf("====================================================\n");
     printf("        HARRY CLARK - ZILOG Z80 EMULATOR            \n");
     printf("====================================================\n");
+
+    CPU_Z80 Z80;
+    Z80_INIT(&Z80);
+
+    printf("Z80 is Running: %p\n", (void*)&Z80);
+
+    U8 TEST_PROGRAM[] = 
+    {
+        0x3E, 0x42,       // LD A, 0x42
+        0x06, 0x10,       // LD B, 0x10
+        0x0E, 0x20,       // LD C, 0x20
+        0x76              // HALT
+    };
+
+    LOAD_TEST_PROGRAM(&Z80, TEST_PROGRAM, sizeof(TEST_PROGRAM));
+
+    while (1)
+    {
+        U8 OPCODE = READ_8(&Z80, Z80.PC);
+        Z80.PC++;
+
+        EXECUTE_OPCODE(&Z80, OPCODE);
+
+        if (Z80.HALT)
+        {
+            break;
+        }
+    }
+
+    printf("Final CPU State:\n");
+    Z80_OUTPUT(&Z80);
+
+    for (int INDEX = 0; INDEX < 64; INDEX++)
+    {
+        free(Z80.READ_MAPPER[INDEX]);
+    }
 
     return 0;
 }
