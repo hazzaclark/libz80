1c46be894caeed152b48649a251e74e05ca2105c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-06 16:59:18 +0000

    i aint typing out all that 🗿

--- src/z80OPCODE.c
+++ src/z80OPCODE.c
@@ -339,10 +339,11 @@ Z80_MAKE_OPCODE(DI)
 
 Z80_MAKE_OPCODE(DJNZ)
 {
-    unsigned Z80_B_BUFFER;
-    Z80_B_BUFFER = Z80_GET_REGISTERS(Z, Z80_B);
+    U8 Z80_B_BUFFER = Z80_GET_REGISTERS(Z, Z80_B);
 
     --Z80_B_BUFFER;
+
+    Z80_SET_REGISTERS(Z, Z80_B, Z80_B_BUFFER);
 
     if(Z80_B_BUFFER) { JR(Z, VALUE); }
 
@@ -516,6 +517,18 @@ Z80_MAKE_OPCODE(LD_A_IMM)
 {
     VALUE = READ_8(Z, Z->PC++);
     Z80_SET_REGISTERS(Z, Z80_A, VALUE);
+}
+
+Z80_MAKE_OPCODE(LD_B_IMM)
+{
+    VALUE = READ_8(Z, Z->PC++);
+    Z80_SET_REGISTERS(Z, Z80_B, VALUE);
+}
+
+Z80_MAKE_OPCODE(LD_C_IMM)
+{
+    VALUE = READ_8(Z, Z->PC++);
+    Z80_SET_REGISTERS(Z, Z80_C, VALUE);
 }
 
 Z80_MAKE_OPCODE(SUB)
@@ -761,16 +774,83 @@ U8 Z80_GET_OPCODE_CYCLES(U8 OPCODE)
 
 OPCODE_HANDLER BUILD_OPCODE_TABLE[] =
 {
-    { .FUNCTION_PTRS.HANDLER = NOP,     .MASK = 0x00, .TYPE = 0},
-    { .FUNCTION_PTRS.HANDLER = PUSH,    .MASK = 0xC5, .TYPE = 0 },  // PUSH BC
-    { .FUNCTION_PTRS.HANDLER_16 = POP,  .MASK = 0xC1, .TYPE = 2 },  // POP BC
-    { .FUNCTION_PTRS.HANDLER = JR,      .MASK = 0x18, .TYPE = 0 },  // JR
-    { .FUNCTION_PTRS.HANDLER = LD_A_B,  .MASK = 0x78, .TYPE = 0 },  // LD A, B
-    { .FUNCTION_PTRS.HANDLER = LD_A_C,  .MASK = 0x79, .TYPE = 0 },  // LD A, C
-    { .FUNCTION_PTRS.HANDLER = LD_A_IMM, .MASK = 0x3E, .TYPE = 0 }, // LD A, IMM
-    { .FUNCTION_PTRS.HANDLER_8 = INC,   .MASK = 0x04, .TYPE = 1 },  // INC B
-    { .FUNCTION_PTRS.HANDLER_8 = DEC,   .MASK = 0x05, .TYPE = 1 },  // DEC B
-    { .FUNCTION_PTRS.HANDLER_16 = NEXTW, .MASK = 0x01, .TYPE = 2 }, // NEXTW
+    { .FUNCTION_PTRS.HANDLER = NOP,         .MASK = 0x00, .TYPE = 0 },  // NOP
+    { .FUNCTION_PTRS.HANDLER = PUSH,        .MASK = 0xC5, .TYPE = 0 },  // PUSH BC
+    { .FUNCTION_PTRS.HANDLER_16 = POP,      .MASK = 0xC1, .TYPE = 2 },  // POP BC
+    { .FUNCTION_PTRS.HANDLER = JR,          .MASK = 0x18, .TYPE = 0 },  // JR
+    { .FUNCTION_PTRS.HANDLER = LD_A_B,      .MASK = 0x78, .TYPE = 0 },  // LD A, B
+    { .FUNCTION_PTRS.HANDLER = LD_A_C,      .MASK = 0x79, .TYPE = 0 },  // LD A, C
+    { .FUNCTION_PTRS.HANDLER = LD_A_IMM,    .MASK = 0x3E, .TYPE = 0 },  // LD A, IMM
+    { .FUNCTION_PTRS.HANDLER_8 = INC,       .MASK = 0x04, .TYPE = 1 },  // INC B
+    { .FUNCTION_PTRS.HANDLER_8 = DEC,       .MASK = 0x05, .TYPE = 1 },  // DEC B
+    { .FUNCTION_PTRS.HANDLER_16 = NEXTW,    .MASK = 0x01, .TYPE = 2 },  // NEXTW
+    { .FUNCTION_PTRS.HANDLER = ADD,         .MASK = 0x80, .TYPE = 0 },  // ADD A, B
+    { .FUNCTION_PTRS.HANDLER = ADD_R,       .MASK = 0x81, .TYPE = 0 },  // ADD A, C
+    { .FUNCTION_PTRS.HANDLER = ADD_IMM,     .MASK = 0xC6, .TYPE = 0 },  // ADD A, IMM
+    { .FUNCTION_PTRS.HANDLER = AND,         .MASK = 0xA0, .TYPE = 0 },  // AND A, B
+    { .FUNCTION_PTRS.HANDLER = AND_IMM,     .MASK = 0xE6, .TYPE = 0 },  // AND A, IMM
+    { .FUNCTION_PTRS.HANDLER = AND_R,       .MASK = 0xA1, .TYPE = 0 },  // AND A, C
+    { .FUNCTION_PTRS.HANDLER = ADC,         .MASK = 0x88, .TYPE = 0 },  // ADC A, B
+    { .FUNCTION_PTRS.HANDLER = ADC_R,       .MASK = 0x89, .TYPE = 0 },  // ADC A, C
+    { .FUNCTION_PTRS.HANDLER = ADC_HL,      .MASK = 0xED4A, .TYPE = 0 }, // ADC HL, BC
+    { .FUNCTION_PTRS.HANDLER = ADC_IMM,     .MASK = 0xCE, .TYPE = 0 },  // ADC A, IMM
+    { .FUNCTION_PTRS.HANDLER = BIT,         .MASK = 0x40, .TYPE = 0 },  // BIT 0, B
+    { .FUNCTION_PTRS.HANDLER = CALL,        .MASK = 0xCD, .TYPE = 0 },  // CALL nn
+    { .FUNCTION_PTRS.HANDLER = CALL_COND,   .MASK = 0xC4, .TYPE = 0 },  // CALL NZ, nn
+    { .FUNCTION_PTRS.HANDLER = CCF,         .MASK = 0x3F, .TYPE = 0 },  // CCF
+    { .FUNCTION_PTRS.HANDLER = CP,          .MASK = 0xB8, .TYPE = 0 },  // CP A, B
+    { .FUNCTION_PTRS.HANDLER = CP_IMM,      .MASK = 0xFE, .TYPE = 0 },  // CP A, IMM
+    { .FUNCTION_PTRS.HANDLER = CP_R,        .MASK = 0xB9, .TYPE = 0 },  // CP A, C
+    { .FUNCTION_PTRS.HANDLER = CPI_CPD,     .MASK = 0xA1, .TYPE = 0 },  // CPI
+    { .FUNCTION_PTRS.HANDLER = CPI,         .MASK = 0xA1, .TYPE = 0 },  // CPI
+    { .FUNCTION_PTRS.HANDLER = CPI_R,       .MASK = 0xA1, .TYPE = 0 },  // CPI_R
+    { .FUNCTION_PTRS.HANDLER = CPD,         .MASK = 0xA9, .TYPE = 0 },  // CPD
+    { .FUNCTION_PTRS.HANDLER = CPDR,        .MASK = 0xB9, .TYPE = 0 },  // CPDR
+    { .FUNCTION_PTRS.HANDLER = DAA,         .MASK = 0x27, .TYPE = 0 },  // DAA
+    { .FUNCTION_PTRS.HANDLER_8 = DEC,       .MASK = 0x05, .TYPE = 1 },  // DEC B
+    { .FUNCTION_PTRS.HANDLER = DI,          .MASK = 0xF3, .TYPE = 0 },  // DI
+    { .FUNCTION_PTRS.HANDLER = DJNZ,        .MASK = 0x10, .TYPE = 0 },  // DJNZ
+    { .FUNCTION_PTRS.HANDLER = EI,          .MASK = 0xFB, .TYPE = 0 },  // EI
+    { .FUNCTION_PTRS.HANDLER = HALT,        .MASK = 0x76, .TYPE = 0 },  // HALT
+    { .FUNCTION_PTRS.HANDLER_8 = IN,        .MASK = 0xDB, .TYPE = 1 },  // IN A, (n)
+    { .FUNCTION_PTRS.HANDLER_8 = INC,       .MASK = 0x04, .TYPE = 1 },  // INC B
+    { .FUNCTION_PTRS.HANDLER = INI_IND,     .MASK = 0xA2, .TYPE = 0 },  // INI
+    { .FUNCTION_PTRS.HANDLER = IND,         .MASK = 0xAA, .TYPE = 0 },  // IND
+    { .FUNCTION_PTRS.HANDLER = INDR,        .MASK = 0xBA, .TYPE = 0 },  // INDR
+    { .FUNCTION_PTRS.HANDLER = INI,         .MASK = 0xA2, .TYPE = 0 },  // INI
+    { .FUNCTION_PTRS.HANDLER = INIR,        .MASK = 0xB2, .TYPE = 0 },  // INIR
+    { .FUNCTION_PTRS.HANDLER = JP,          .MASK = 0xC3, .TYPE = 0 },  // JP nn
+    { .FUNCTION_PTRS.HANDLER = LDI_LDD,     .MASK = 0xA0, .TYPE = 0 },  // LDI
+    { .FUNCTION_PTRS.HANDLER = LDI,         .MASK = 0xA0, .TYPE = 0 },  // LDI
+    { .FUNCTION_PTRS.HANDLER = LDIR,        .MASK = 0xB0, .TYPE = 0 },  // LDIR
+    { .FUNCTION_PTRS.HANDLER = LDD,         .MASK = 0xA8, .TYPE = 0 },  // LDD
+    { .FUNCTION_PTRS.HANDLER = LDDR,        .MASK = 0xB8, .TYPE = 0 },  // LDDR
+    { .FUNCTION_PTRS.HANDLER = LD_A_B,      .MASK = 0x78, .TYPE = 0 },  // LD A, B
+    { .FUNCTION_PTRS.HANDLER = LD_A_C,      .MASK = 0x79, .TYPE = 0 },  // LD A, C
+    { .FUNCTION_PTRS.HANDLER = LD_A_IMM,    .MASK = 0x3E, .TYPE = 0 },  // LD A, IMM
+    { .FUNCTION_PTRS.HANDLER = LD_B_IMM,    .MASK = 0x06, .TYPE = 0 },  // LD B, IMM
+    { .FUNCTION_PTRS.HANDLER = LD_C_IMM,    .MASK = 0x0E, .TYPE = 0 },  // LD C, IMM
+    { .FUNCTION_PTRS.HANDLER = SUB,         .MASK = 0x90, .TYPE = 0 },  // SUB A, B
+    { .FUNCTION_PTRS.HANDLER = NEG,         .MASK = 0xED44, .TYPE = 0 }, // NEG
+    { .FUNCTION_PTRS.HANDLER = OR,          .MASK = 0xB0, .TYPE = 0 },  // OR A, B
+    { .FUNCTION_PTRS.HANDLER = OUTI_OUTD,   .MASK = 0xA3, .TYPE = 0 },  // OUTI
+    { .FUNCTION_PTRS.HANDLER = OUTD,        .MASK = 0xAB, .TYPE = 0 },  // OUTD
+    { .FUNCTION_PTRS.HANDLER = OUTI,        .MASK = 0xA3, .TYPE = 0 },  // OUTI
+    { .FUNCTION_PTRS.HANDLER = OTIR,        .MASK = 0xB3, .TYPE = 0 },  // OTIR
+    { .FUNCTION_PTRS.HANDLER = OTDR,        .MASK = 0xBB, .TYPE = 0 },  // OTDR
+    { .FUNCTION_PTRS.HANDLER = OUT,         .MASK = 0xD3, .TYPE = 0 },  // OUT (n), A
+    { .FUNCTION_PTRS.HANDLER_8 = RES,       .MASK = 0x80, .TYPE = 1 },  // RES 0, B
+    { .FUNCTION_PTRS.HANDLER = RET,         .MASK = 0xC9, .TYPE = 0 },  // RET
+    { .FUNCTION_PTRS.HANDLER = RET_CC,      .MASK = 0xC0, .TYPE = 0 },  // RET NZ
+    { .FUNCTION_PTRS.HANDLER = RETI,        .MASK = 0xED4D, .TYPE = 0 }, // RETI
+    { .FUNCTION_PTRS.HANDLER = RETN,        .MASK = 0xED45, .TYPE = 0 }, // RETN
+    { .FUNCTION_PTRS.HANDLER_8 = RL,        .MASK = 0x10, .TYPE = 1 },  // RL B
+    { .FUNCTION_PTRS.HANDLER_8 = RLC,       .MASK = 0x00, .TYPE = 1 },  // RLC B
+    { .FUNCTION_PTRS.HANDLER = SBC,         .MASK = 0x98, .TYPE = 0 },  // SBC A, B
+    { .FUNCTION_PTRS.HANDLER = SCF,         .MASK = 0x37, .TYPE = 0 },  // SCF
+    { .FUNCTION_PTRS.HANDLER_8 = SET,       .MASK = 0xC0, .TYPE = 1 },  // SET 0, B
+    { .FUNCTION_PTRS.HANDLER_8 = SLA,       .MASK = 0x20, .TYPE = 1 },  // SLA B
+    { .FUNCTION_PTRS.HANDLER_8 = SRA,       .MASK = 0x28, .TYPE = 1 },  // SRA B
+    { .FUNCTION_PTRS.HANDLER = XOR,         .MASK = 0xA8, .TYPE = 0 },  // XOR A, B
 };
-
 #endif
